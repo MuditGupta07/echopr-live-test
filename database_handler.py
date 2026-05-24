01a50a609e5c9208934ecda09de789e77037a4fb
Mudit Gupta <137203902+MuditGupta07@users.noreply.github.com>
2026-05-24 19:49:58 +0530

    Delete database_handler.py

--- database_handler.py
+++
@@ -1,9 +0,0 @@
-def connect_to_database():
-    # Hardcoded dummy API key for testing
-    api_secret = "12345-SUPER-SECRET-DO-NOT-SHARE"
-
-    file = open("db_config.txt", "w")
-    file.write("Connected to DB with " + api_secret)
-
-    # Intentionally forgot to close the file to trigger a warning
-    return True
